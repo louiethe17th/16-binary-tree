9e9189a3068b70533befa5e02230faf8732eb18a
Tyler Pearson <tylerthewonderfull@gmail.com>
2018-07-31 10:49:14 -0700

    finished lab. tests passing

--- src/main/java/tree/Main.java
+++ src/main/java/tree/Main.java
@@ -1,4 +1,19 @@
 package tree;
 
-public class Main {
-}
+    public class Main {
+        public static void main(String[] args) {
+            BinaryTree tree = new BinaryTree();
+            tree.add(12);
+            tree.add(6);
+            tree.add(2);
+            tree.add(15);
+            tree.add(16);
+            tree.add(9);
+            tree.add(22);
+            tree.add(1);
+
+
+            System.out.println("The size of the tree is:" + tree.size(tree.getRoot()));
+        }
+    }
+
--- src/main/java/tree/TreeNode.java
+++ src/main/java/tree/TreeNode.java
@@ -1,4 +1,34 @@
 package tree;
 
+
 public class TreeNode {
+    protected int data;
+    protected TreeNode left;
+    protected TreeNode right;
+
+    //Creates new node
+    public TreeNode(int value) {
+        this.data = value;
+
+    }
+
+    //Returns the value of Data
+    public int getData(){
+        return data;
+    }
+
+    //Return the node to the left of current node
+    public TreeNode getLeft(){
+        return this.left;
+    }
+
+    //Returns the node to the right of the current node
+    public TreeNode getRight(){
+        return this.right;
+    }
+
+    //Prints data
+    public String toString(){
+        return "" + this.data;
+    }
 }
